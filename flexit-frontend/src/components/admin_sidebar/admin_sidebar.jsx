1eee22fdfaeea0d7d3af416b92dc753714d6107a
WMPL02 <prabodalakshan02@gmail.com>
2026-04-19 13:11:52 +0530

    add ticket menagmet to admin side bar and fix css

--- flexit-frontend/src/components/admin_sidebar/admin_sidebar.jsx
+++ flexit-frontend/src/components/admin_sidebar/admin_sidebar.jsx
@@ -39,6 +39,14 @@ function AdminSidebar() {
               Resource Management
             </Link>
 
+            <Link
+              to="/admin/bookings"
+              className={getLinkClasses('/admin/bookings')}
+            >
+              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10m-11 9h12a2 2 0 002-2V7a2 2 0 00-2-2H6a2 2 0 00-2 2v11a2 2 0 002 2z" /></svg>
+              Booking Management
+            </Link>
+
             <Link 
               to="/admin/tickets" 
               className={getLinkClasses('/admin/tickets')}
